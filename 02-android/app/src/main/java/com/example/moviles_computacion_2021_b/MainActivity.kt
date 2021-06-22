a9114ff90b0479179265db474e45714c8b0c7625
Danny <daniela.jacome@epn.edu.ec>
2021-06-22 11:25:22 -0500

    Actividad Empty
    Ciclo de vida de las actividades, tipos de layout

--- 02-android/app/src/main/java/com/example/moviles_computacion_2021_b/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/moviles_computacion_2021_b/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.moviles_computacion_2021_b
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Extrae el boton a traves del id
+        val botonIrACicloVida = findViewById<Button>(
+            R.id.btn_ciclo_vida
+        )
+
+        botonIrACicloVida.setOnClickListener {
+            abrirCicloVida()
+        }
+    }
+
+    fun abrirCicloVida(){
+        val intentExplicito = Intent(
+            this,
+            ACicloVida::class.java
+        )
+        startActivity(intentExplicito)
     }
 }
+
